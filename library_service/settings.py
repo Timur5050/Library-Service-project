406942507f225ab46a43bc2cf0f12c99df9e75a8
timur <stukantimur811@gmail.com>
2024-08-15 09:14:45 +0300

    added drf to django installed apps

--- library_service/settings.py
+++ library_service/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "book_service"
+    "book_service",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
