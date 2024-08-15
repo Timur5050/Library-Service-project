dbf0c0f84fffd724e43d987b8d4eabc9fc14f5dd
timur <stukantimur811@gmail.com>
2024-08-15 11:37:54 +0300

    added persmission class readonlyoradminpermission for book view set

--- book_service/views.py
+++ book_service/views.py
@@ -2,8 +2,11 @@ from rest_framework import viewsets
 
 from book_service.models import Book
 from book_service.serializers import BookSerializer
+from book_service.permissions import ReadOnlyOrAdminPermission
 
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = (ReadOnlyOrAdminPermission, )
+
