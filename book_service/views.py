1fab551c83b699d1beb0774445a1270d02001fde
timur <stukantimur811@gmail.com>
2024-08-15 09:24:18 +0300

    create view set for book

--- book_service/views.py
+++ book_service/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from book_service.models import Book
+from book_service.serializers import BookSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
