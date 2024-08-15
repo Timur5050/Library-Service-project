71b4a94b7c4a63362bf30891a871c76a9e378204
timur <stukantimur811@gmail.com>
2024-08-15 13:19:50 +0300

    updated borriw view and added borrowing books opportunity

--- borrow_service/views.py
+++ borrow_service/views.py
@@ -1,22 +1,41 @@
+from django.http import Http404
 from rest_framework import mixins
 from rest_framework.generics import GenericAPIView, RetrieveAPIView
 
+from book_service.models import Book
 from borrow_service.models import Borrow
 from borrow_service.serializers import (
     BorrowListSerializer,
-    BorrowRetrieveSerializer
+    BorrowRetrieveSerializer, BorrowCreateSerializer
 )
 
 
 class BorrowListView(
     mixins.ListModelMixin,
+    mixins.CreateModelMixin,
     GenericAPIView
 ):
     queryset = Borrow.objects.all()
-    serializer_class = BorrowListSerializer
+    serializer = BorrowListSerializer
+
+    def get_serializer_class(self):
+        serializer = self.serializer
+        if self.request.method == "POST":
+            serializer = BorrowCreateSerializer
+
+        return serializer
 
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        book = Book.objects.get(pk=request.data["book"])
+        if book.inventory > 0:
+            book.inventory -= 1
+            book.save()
+            return self.create(request, *args, **kwargs)
+
+        raise Http404("No such books available")
 
 
 class BorrowRetrieveView(
