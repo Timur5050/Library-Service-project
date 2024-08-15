142ecf98249325ad07b99f233202dba4adb4ab8e
timur <stukantimur811@gmail.com>
2024-08-15 12:34:12 +0300

    create views for list and detail borrows

--- borrow_service/views.py
+++ borrow_service/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework.generics import GenericAPIView, RetrieveAPIView
 
-# Create your views here.
+from borrow_service.models import Borrow
+from borrow_service.serializers import (
+    BorrowListSerializer,
+    BorrowRetrieveSerializer
+)
+
+
+class BorrowListView(
+    mixins.ListModelMixin,
+    GenericAPIView
+):
+    queryset = Borrow.objects.all()
+    serializer_class = BorrowListSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+
+class BorrowRetrieveView(
+    mixins.RetrieveModelMixin,
+    GenericAPIView
+):
+    serializer_class = BorrowRetrieveSerializer
+
+    def get_object(self):
+        return Borrow.objects.get(id=self.kwargs["pk"])
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
