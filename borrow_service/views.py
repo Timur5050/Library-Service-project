4abd187440178e33ed7ec9d926cd246a6484698d
timur <stukantimur811@gmail.com>
2024-08-15 19:44:51 +0300

    moved notifications to notifications service

--- borrow_service/views.py
+++ borrow_service/views.py
@@ -17,19 +17,7 @@ from borrow_service.serializers import (
     BorrowRetrieveSerializer, BorrowCreateSerializer
 )
 
-
-def send_message_to_telegram_group(message: str):
-    bot_token = settings.TELEGRAM_BOT_TOKEN
-    chat_id = settings.TELEGRAM_CHAT_ID
-    message = message
-
-    url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
-
-    payload = {
-        "chat_id": chat_id,
-        "text": message
-    }
-    return requests.post(url, data=payload)
+from notifications_service.views import send_message_to_telegram_group
 
 
 class BorrowListView(
--- notifications_service/views.py
+++ notifications_service/views.py
@@ -1,3 +1,17 @@
+import requests
+from django.conf import settings
 from django.shortcuts import render
 
-# Create your views here.
+
+def send_message_to_telegram_group(message: str):
+    bot_token = settings.TELEGRAM_BOT_TOKEN
+    chat_id = settings.TELEGRAM_CHAT_ID
+    message = message
+
+    url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
+
+    payload = {
+        "chat_id": chat_id,
+        "text": message
+    }
+    return requests.post(url, data=payload)
