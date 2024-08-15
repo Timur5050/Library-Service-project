f4428027a9d47db844a942aaf5cdef3974f284bd
timur <stukantimur811@gmail.com>
2024-08-15 15:31:20 +0300

    created endpoint for returning status

--- borrow_service/urls.py
+++ borrow_service/urls.py
@@ -3,11 +3,13 @@ from django.urls import path
 from borrow_service.views import (
     BorrowListView,
     BorrowRetrieveView,
+    return_borrowed_book
 )
 
 urlpatterns = [
     path("borrows/", BorrowListView.as_view(), name="borrows"),
     path("borrows/<int:pk>/", BorrowRetrieveView.as_view(), name="borrows"),
+    path("borrow/<int:borrow_id>/return/", return_borrowed_book, name="return_borrowed_book"),
 ]
 
 app_name = "borrow_service"
