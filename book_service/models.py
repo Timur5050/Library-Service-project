37f4ef0b4245fd5550b64ca314f70c77115ad3dc
timur <stukantimur811@gmail.com>
2024-08-15 09:27:57 +0300

    fixes

--- book_service/models.py
+++ book_service/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Book(models.Model):
     class Cover(models.TextChoices):
-        HARD = ("HARD",)
+        HARD = "HARD"
         SOFT = "SOFT"
 
     title = models.CharField(max_length=100)
