f0e6256e97f8b89b9444fd3a51ce2b5cf15cd6f6
timur <stukantimur811@gmail.com>
2024-08-18 14:45:06 +0300

    added url patterns to documentation

--- library_service/urls.py
+++ library_service/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -24,4 +25,6 @@ urlpatterns = [
     path("api/v1/", include("user_service.urls", namespace="user_service")),
     path("api/v1/", include("borrow_service.urls", namespace="borrow_service")),
     path("api/v1/", include("payments_service.urls", namespace="payment_service")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/doc/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
 ] + debug_toolbar_urls()
