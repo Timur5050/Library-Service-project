f69ff21ff52781abd24a1388dbe0b6a105f70639
timur <stukantimur811@gmail.com>
2024-08-18 14:46:02 +0300

    added permission class to user update their profile

--- user_service/views.py
+++ user_service/views.py
@@ -1,3 +1,5 @@
+from rest_framework.permissions import IsAuthenticated
+
 from user_service.serializers import UserCreateSerializer, UserUpdateSerializer
 
 from rest_framework.generics import CreateAPIView, RetrieveUpdateAPIView
@@ -9,6 +11,7 @@ class CreateUserView(CreateAPIView):
 
 class UpdateRetrieveUserView(RetrieveUpdateAPIView):
     serializer_class = UserUpdateSerializer
+    permission_classes = (IsAuthenticated,)
 
     def get_object(self):
         return self.request.user
