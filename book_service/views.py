bd9fff647c0a3b20816dea04ecf297bcdf27499b
timur <stukantimur811@gmail.com>
2024-08-18 15:09:28 +0300

    documented book views

--- book_service/views.py
+++ book_service/views.py
@@ -1,3 +1,4 @@
+from drf_spectacular.utils import extend_schema_view, extend_schema
 from rest_framework import viewsets
 
 from book_service.models import Book
@@ -5,8 +6,27 @@ from book_service.serializers import BookSerializer
 from book_service.permissions import ReadOnlyOrAdminPermission
 
 
+@extend_schema_view(
+    list=extend_schema(
+        summary="get list of books, allowed to everyone",
+    ),
+    retrieve=extend_schema(
+        summary="retrieve book, allowed to everyone"
+    ),
+    update=extend_schema(
+        summary="update book, allowed to admins only"
+    ),
+    partial_update=extend_schema(
+        summary="partial update book, allowed to admins only",
+    ),
+    create=extend_schema(
+        summary="create new book, allowed to admins only",
+    ),
+    destroy=extend_schema(
+        summary="delete book, allowed to admins only",
+    ),
+)
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = (ReadOnlyOrAdminPermission, )
-
+    permission_classes = (ReadOnlyOrAdminPermission,)
