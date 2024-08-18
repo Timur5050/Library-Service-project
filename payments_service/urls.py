a5040406702c4c25b08ba17d13410a566211c0bb
timur <stukantimur811@gmail.com>
2024-08-18 13:17:38 +0300

    created endpoints for fine success and cancel paymants

--- payments_service/urls.py
+++ payments_service/urls.py
@@ -5,13 +5,17 @@ from payments_service.views import (
     PaymentRetrieveView,
     success_payment_session,
     cancel_payment_session,
+    success_fine_session,
+    cancel_fine_session
 )
 
 urlpatterns = [
     path("payments/", PaymentListView.as_view(), name="payments-list"),
     path("payments/<int:pk>/", PaymentRetrieveView.as_view(), name="payments-retrieve"),
-    path("success/<int:payment_id>/", success_payment_session, name="payments-success"),
-    path("cancel/<int:payment_id>/", cancel_payment_session, name="payments-cancel"),
+    path("success/payment/<int:payment_id>/", success_payment_session, name="payments-success"),
+    path("cancel/payment/<int:payment_id>/", cancel_payment_session, name="payments-cancel"),
+    path("success/fine/<int:payment_id>/", success_fine_session, name="fine-success"),
+    path("cancel/fine/", cancel_fine_session, name="fine-cancel"),
 ]
 
 app_name = "payment_service"
