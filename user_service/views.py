e4ef54d24d9ad4b87a8ebac2064c4cffdb12147b
timur <stukantimur811@gmail.com>
2024-08-18 15:10:21 +0300

    documented user views

--- user_service/views.py
+++ user_service/views.py
@@ -1,3 +1,4 @@
+from drf_spectacular.utils import extend_schema_view, extend_schema
 from rest_framework.permissions import IsAuthenticated
 
 from user_service.serializers import UserCreateSerializer, UserUpdateSerializer
@@ -8,6 +9,13 @@ from rest_framework.generics import CreateAPIView, RetrieveUpdateAPIView
 class CreateUserView(CreateAPIView):
     serializer_class = UserCreateSerializer
 
+    @extend_schema(
+        summary="register new user",
+        responses={200: UserCreateSerializer(many=False)}
+    )
+    def post(self, request, *args, **kwargs):
+        return super().create(request, *args, **kwargs)
+
 
 class UpdateRetrieveUserView(RetrieveUpdateAPIView):
     serializer_class = UserUpdateSerializer
@@ -15,3 +23,24 @@ class UpdateRetrieveUserView(RetrieveUpdateAPIView):
 
     def get_object(self):
         return self.request.user
+
+    @extend_schema(
+        summary="retrieve your profile",
+        responses={200: UserUpdateSerializer(many=False)}
+    )
+    def get(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
+
+    @extend_schema(
+        summary="update your profile",
+        responses={200: UserUpdateSerializer(many=False)}
+    )
+    def put(self, request, *args, **kwargs):
+        return super().update(request, *args, **kwargs)
+
+    @extend_schema(
+        summary="partial update your profile",
+        responses={200: UserUpdateSerializer(many=False)}
+    )
+    def patch(self, request, *args, **kwargs):
+        return super().partial_update(request, *args, **kwargs)
